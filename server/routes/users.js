fd39f39f878d784ed6022fab825dbe6cc084df89
ekifakhrureza <ekifakhrureza@gmail.com>
2018-04-12 14:26:38 +0700

    PUSH KEDUA

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const {register,login} =  require('../controllers/user.controller')
 
-module.exports = router;
+router.post('/register',register)
+router.post('/login',login)
+
+module.exports = router
